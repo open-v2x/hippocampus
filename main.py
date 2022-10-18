18730c6e6ec84fccc371fa24e6d0417011654fe9
gaotianyu <gao.tianyu@99cloud.net>
2022-10-18 17:19:15 +0800

    feat: use tensorrt to speedup inference process

--- main.py
+++ main.py
@@ -17,20 +17,17 @@ def func(camera):
 
     while True:
         det = Detector()
-        # cap = cv2.VideoCapture(os.getenv('rtsp') or 'videos/test.mp4')
         cap = cv2.VideoCapture(os.getenv('rtsp') or camera["camera_id"])
         size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
         sizeStr = str(size[0]) + 'x' + str(size[1])
-        # rtmp = os.getenv('rtmp') or 'rtmp://localhost:1935/live/rfBd56ti2SMtYvSgD5xAV0YU99zampta7Z7S575KLkIZ9PYk'
         rtmp="rtmp://localhost:1935/live/" + channlkey
-        rtmp = rtmp or 'rtmp://localhost:1935/live/rfBd56ti2SMtYvSgD5xAV0YU99zampta7Z7S575KLkIZ9PYk'
         command = ['ffmpeg',
                 '-y',
                 '-stream_loop', '-1',
                 '-f', 'rawvideo',
                 '-vcodec', 'rawvideo',
                 '-pix_fmt', 'bgr24',
-                '-s', '480x270',
+                '-s', '720x480',
                 '-r', '25',
                 '-i', '-',
                 '-c:v', 'libx264',
@@ -38,17 +35,16 @@ def func(camera):
                 '-preset', 'ultrafast',
                 '-f', 'flv',
                 rtmp]
-        ffrtsp = subprocess.Popen(command, stdin=subprocess.PIPE)
+        pipe = subprocess.Popen(command, stdin=subprocess.PIPE)
 
         while True:
             ret, im = cap.read()
             if not ret:
                 break
-            im_rgb = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
-            im = cv2.resize(im_rgb, (480, 270), interpolation=cv2.INTER_LINEAR)
+            im = cv2.resize(im_rgb, (720, 480), interpolation=cv2.INTER_LINEAR)
             result = det.feedCap(im)
             result = result['frame']
-            ffrtsp.stdin.write(result.tobytes())
+            pipe.stdin.write(result.tobytes())
 
         cap.release()
 
